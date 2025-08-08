af1e07363c61a1b519e9c12ab1f1d08dde9718f8
MarMar Tahery <mtahery20@hotmail.com>
2025-08-08 00:05:32 -0700

    Random text for pages

--- src/About.js
+++ src/About.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 function About() {
-  return (
-    <section>
-      <h2>About Me</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-    </section>
-  );
+    return (
+        <section>
+            <h2>About Me</h2>
+            <p>
+                MarMar joined Abbott in 2024 as an Associate Systems Engineer in RCPE. She has helped resolve multiple field complaints while mitigating ongoing production issues and streamlining upcoming projects simultaneously. She works across multiple departments to ensure that customers and patients are giving and receiving appropriate and necessary healthcare at all times while ensuring any and all new products entering production are satisfactory on all aspects. She has helped improve Tiger Ticket SLAs, mentored and trained new RCPE hires, met all MCR completion deadlines, and has lead the ongoing EU migration efforts for RCPE.
+                Prior to joining Abbott, MarMar was a website developer who specialized in creating resources for both adolescents and adults struggling with an array of hardships throughout Los Angeles County and Orange County. Ranging from foster care, domestic violence, mental health, and drug/alcohol abuse, MarMar would create and develop websites holding valuable resources and aids for individuals experiencing these hardships. She ensures that her products are not only working properly on a technical level but also on a personal level. By conducting market research and collecting analytics on those who utilize the city-funded sites, she adjusts the resources to fit the needs of all who find themselves requesting extra assistance.
+                MarMar attended California State University, Northridge and holds a Bachelor's of Science in Computer Science as well as a minor in Computational Mathematics. She enjoys building Legos, knitting sweaters for her cats, streaming videogames on Twitch, and rewatching the entire Harry Potter series to no end.
+            </p>
+        </section>
+    );
 }
 
 export default About;
--- src/Home.js
+++ src/Home.js
@@ -4,8 +4,7 @@ function Home() {
   return (
     <section>
       <h2>Welcome to My Portfolio</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      {/* Your existing landing page content goes here */}
+      <p>Hi there! Excuse our dust, but we're currently under construction. Check back later when we've gotten a well-deserved face lift!</p>
     </section>
   );
 }
